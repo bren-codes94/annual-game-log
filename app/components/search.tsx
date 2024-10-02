c03dcb85d2dd2c053318f902439748ffd4b279bd
Brendan Ryan <bren.codes94@gmail.com>
2024-10-02 17:48:00 -0400

    added debounce to search box

--- app/components/search.tsx
+++ app/components/search.tsx
@@ -1,9 +1,16 @@
 import { Description, Field, Input, Label } from '@headlessui/react'
 import clsx from 'clsx'
 
-// TODO: when user types, expose result to parent component (debounced)
-
-export default function Search() {
+export default function Search({onChange}: any) {
+  // debounce event listener funciton call
+  let onChangeTimeout: any; 
+  function handleChange(e: any) {
+    clearTimeout(onChangeTimeout);
+    onChangeTimeout = setTimeout(
+      () => onChange(e)
+      , 750
+    )
+  }
   return (
     <div className="w-full max-w-md">
       <Field>
@@ -13,6 +20,7 @@ export default function Search() {
             'mt-3 block w-full rounded-lg border-none bg-white/5 py-1.5 px-3 text-sm/6 text-white',
             'focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-white/25'
           )}
+          onChange={handleChange}
         />
       </Field>
     </div>
--- app/newgamelog/page.tsx
+++ app/newgamelog/page.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import Search from "@/app/components/search";
 import { searchGames } from "@/data/igdb";
+import { useState } from "react";
 
 // TODO: when a new value is emitted from Search component, query IGDB API for list of games
 interface IGDBGames {
@@ -8,28 +11,23 @@ interface IGDBGames {
   genres: any;
 }
 
-export default async function NewGameLog() {
-  let data = await searchGames('halo');
-  let games: IGDBGames[] = await data.json();
+export default function NewGameLog() {
+  // let data = await searchGames('halo');
+  // let games: IGDBGames[] = await data.json();
+
+  const [searchText, setSearchText] = useState("");
+
+  function handleChange(e: any) {
+    setSearchText(e.target.value);
+  }
   return (
     <div className="h-full">
       <section className="selectedgames m-8">
         <h2 className="text-2xl">Annual Game Log - Draft</h2>
       </section>
       <section className="searchgames m-8 h-full">
-        <Search />
-        <div className="p-20 flex justify-center">
-          Halo Games:
-          <ul>
-            {games.map((game) => {
-              return(
-                <li key={game.id}>
-                  {game.name}
-                </li>
-              )
-            })}
-          </ul>
-        </div>
+        <Search onChange={handleChange} />
+        {searchText}
         <div className="p-20 flex justify-center">
           Use the search box to find and add games to your log.
         </div>
